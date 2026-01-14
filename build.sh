0ab0f7848edeb05232b0c393be31fefad2317b6b
katheriney25-oss <katherine.y25@gmail.com>
2026-01-13 18:28:28 -0600

    update changes tonight

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "/katheriney25-oss/"
--- src/main.py
+++ src/main.py
@@ -1,40 +1,22 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
-from gencontent import generate_page
+from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
-
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    os.makedirs(dest_dir_path, exist_ok=True)
-
-    for entry in os.listdir(dir_path_content):
-        src_path = os.path.join(dir_path_content, entry)
-        dest_path = os.path.join(dest_dir_path, entry)
-
-        if os.path.isdir(src_path):
-            generate_pages_recursive(
-                src_path,
-                template_path,
-                dest_path
-            )
-
-        elif entry.endswith(".md"):
-            dest_html_path = dest_path.replace(".md", ".html")
-
-            print(f"Generating page: {src_path} -> {dest_html_path}")
-            generate_page(
-                src_path,
-                template_path,
-                dest_html_path
-            )
+default_basepath = "/"
 
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -46,7 +28,8 @@ def main():
     generate_pages_recursive(
         dir_path_content,
         template_path,
-        dir_path_public
+        dir_path_public,
+        basepath
     )
 
 main()
